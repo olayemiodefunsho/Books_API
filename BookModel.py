6a15eaef27b9f81cfdf9bdbf2553c68a0736fea2
olayemi odefunsho <odefunsho.olayemi@gmail.com>
2020-01-30 22:59:07 +0100

    get all books from db

--- BookModel.py
+++ BookModel.py
@@ -11,3 +11,19 @@ class Book(db.Model):
     name = db.Column(db.String(80), nullable=False)
     price = db.Column(db.Float, nullable=False)
     isbn = db.Column(db.Integer)    
+
+    def add_book(_name, _price, _isbn):
+        new_book = Book(name=_name, price=_price, isbn=_isbn)
+        db.session.add(new_book)
+        db.session.commit()
+
+    def get_all_books():
+        return Book.query.all()
+
+    def __repr__(self):
+        book_object = {
+            'name' : self.name,
+            'price' : self.price,
+            'isbn' : self.isbn
+        }
+        return json.dumps(book_object)
