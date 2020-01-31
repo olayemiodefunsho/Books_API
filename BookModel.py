f0d25fb3796f31ee6c53b31d96bdd3fcbe14be9f
olayemi odefunsho <odefunsho.olayemi@gmail.com>
2020-01-31 23:47:11 +0100

    completign the book model

--- BookModel.py
+++ BookModel.py
@@ -10,7 +10,10 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     price = db.Column(db.Float, nullable=False)
-    isbn = db.Column(db.Integer)    
+    isbn = db.Column(db.Integer) 
+
+    def json(self):
+        return{'name': self.name, 'price': self.price, 'isbn': self.isbn}
 
     def add_book(_name, _price, _isbn):
         new_book = Book(name=_name, price=_price, isbn=_isbn)
@@ -18,7 +21,30 @@ class Book(db.Model):
         db.session.commit()
 
     def get_all_books():
-        return Book.query.all()
+        return [Book.json(book) for book in Book.query.all()]
+
+    def get_book(_isbn):
+        return Book.json(Book.query.filter_by(isbn=_isbn).first())
+
+    def delete_book(_isbn):
+        Book.query.filter_by(isbn=_isbn).delete()
+        db.session.commit()
+
+    def update_book_price(_isbn, _price):
+        book_to_update = Book.query.filter_by(isbn=_isbn).first()
+        book_to_update.price = _price
+        db.session.commit()
+
+    def update_book_name(_isbn, _name):
+        book_to_update = Book.query.filter_by(isbn=_isbn).first()
+        book_to_update.name = _name
+        db.session.commit()
+
+    def replace_book(_isbn, _name, _price):
+        book_to_update = Book.query.filter_by(isbn=_isbn).first()
+        book_to_update.name = _name
+        book_to_update.price = _price
+        db.session.commit()
 
     def __repr__(self):
         book_object = {
